c179b6d2b73d22ee4797bbf233c60a914df499a8
conghuule <you@example.com>
2023-09-09 20:01:20 +0700

    config

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const { sequelize } = require("./models");
+const app = express();
+
+//setting app to use express.json() to parse json data
+app.use(express.json());
+
+//setting static file
+const publicPathDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicPathDirectory));
+
+//listen connect
+app.listen(3000, async () => {
+  console.log("App listening on http://localhost:3000");
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+});
